7395e2b673e37fdba135417b901236b4a7c7797f
zagoorland <zagoorland@gmail.com>
2020-12-12 17:54:18 +0100

    added sass and testpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import TestPage from "./Pages/TestPage";
 
 function App() {
-  return <div className="App">hej</div>;
+  return (
+    <div className="App">
+      <TestPage />
+    </div>
+  );
 }
 
 export default App;
